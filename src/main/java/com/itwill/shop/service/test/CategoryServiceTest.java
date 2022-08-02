8bb98cf79f8892682596bd73a88a8ee7fb6a282b
Harim <godto@DESKTOP-5CJT6FD>
2022-08-02 23:49:22 +0900

    22.08.02

--- src/main/java/com/itwill/shop/service/test/CategoryServiceTest.java
+++ src/main/java/com/itwill/shop/service/test/CategoryServiceTest.java
@@ -1,10 +1,14 @@
 package com.itwill.shop.service.test;
 
+import com.itwill.shop.service.CategoryService;
+
 public class CategoryServiceTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		CategoryService categoryService = new CategoryService();
+		
+		//카테고리 이름으로 상품출력
+		
 	}
 
 }
