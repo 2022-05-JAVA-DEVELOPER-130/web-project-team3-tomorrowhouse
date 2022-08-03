5628b2c3aab7bead3eb9fd9f2c3622e2c9be0426
Harim <godtoether94@naver.com>
2022-08-03 17:52:50 +0900

    22.08.03 userid 서비스 테스트 수정

--- src/main/java/com/itwill/shop/service/test/UserInfoServiceTest.java
+++ src/main/java/com/itwill/shop/service/test/UserInfoServiceTest.java
@@ -10,27 +10,27 @@ public class UserInfoServiceTest {
 		UserInfoService userInfoService = new UserInfoService();
 		
 		
-//		System.out.println(">>>>> 회원 가입");
-//		System.out.println(userInfoService.create(new UserInfo("kim", "1111", "김김김", "kim@gmail.com", "평양시", "010-7878-5656")));
-//		
-//		System.out.println(">>>>> 회원 로그인");
-//		System.out.println(userInfoService.login("test1","pw1"));
-//		
+		System.out.println(">>>>> 회원 가입");
+		System.out.println(userInfoService.create(new UserInfo("kim", "1111", "김김김", "kim@gmail.com", "평양시", "010-7878-5656")));
+		
+		System.out.println(">>>>> 회원 로그인");
+		System.out.println(userInfoService.login("test1","pw1"));
+		
 		System.out.println(">>>>> 회원 전체 리스트");
 		System.out.println(userInfoService.findUserInfoList());
-//		
-//		System.out.println(">>>>> 회원 1명 보기");
-//		System.out.println(userInfoService.findUser("test1"));
-//		
-//		System.out.println(">>>>> 회원 정보 수정");
-//		System.out.println(userInfoService.update(new UserInfo(null, null, null, null, null, null)));
-//		
-//		System.out.println(">>>>> 회원 탈퇴");
-//		System.out.println(userInfoService.remove("test3"));
-//		
-//		System.out.println(">>>>> 아이디 중복 체크");
-//		System.out.println(userInfoService.isDuplicateId("test1"));
-//		
+		
+		System.out.println(">>>>> 회원 1명 보기");
+		System.out.println(userInfoService.findUser("test1"));
+		
+		System.out.println(">>>>> 회원 정보 수정");
+		System.out.println(userInfoService.update(new UserInfo("kim", "1111","변경이름","@test.com","평양","010-1111-1111")));
+		
+		System.out.println(">>>>> 회원 탈퇴");
+		System.out.println(userInfoService.remove("kim"));
+		
+		System.out.println(">>>>> 아이디 중복 체크");
+		System.out.println(userInfoService.isDuplicateId("test1"));
+		
 		
 		
 		
