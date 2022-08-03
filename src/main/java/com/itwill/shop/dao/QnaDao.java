53a6c75047d21484c6b6dd236e7e5fd4497575c0
yooning92 <brv19@naver.com>
2022-08-03 12:13:23 +0900

    ..

---
+++ src/main/java/com/itwill/shop/dao/QnaDao.java
@@ -0,0 +1,24 @@
+package com.itwill.shop.dao;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+
+public class QnaDao {
+	private DataSource dataSource;
+	public QnaDao() throws Exception {
+	Properties properties = new Properties();
+	properties.load(this.getClass().getResourceAsStream("/com/itwill/shop/common/jdbc.properties"));
+	/*** Apache DataSource ***/
+	BasicDataSource basicDataSource = new BasicDataSource();
+	basicDataSource.setDriverClassName(properties.getProperty("driverClassName"));
+	basicDataSource.setUrl(properties.getProperty("url"));
+	basicDataSource.setUsername(properties.getProperty("user"));
+	basicDataSource.setPassword(properties.getProperty("password"));
+	dataSource = basicDataSource;
+	}
+	
+	
+}
