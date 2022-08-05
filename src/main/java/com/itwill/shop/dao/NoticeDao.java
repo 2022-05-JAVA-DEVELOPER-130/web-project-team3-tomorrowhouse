eb66e02f1ae806b592ad3e9f8b13ee104daf0014
itwill05 <ignore4557@gmail.com>
2022-08-05 18:04:38 +0900

    ..

--- src/main/java/com/itwill/shop/dao/NoticeDao.java
+++ src/main/java/com/itwill/shop/dao/NoticeDao.java
@@ -97,4 +97,104 @@ public class NoticeDao {
 		return rowCount;
 	}
 	
+	//페이징에필요한거
+	public ArrayList<Notice> findNoticeList(int start, int last)throws Exception{
+		System.out.println(""+start+" ~ "+last);
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<Notice> notices = new ArrayList<Notice>();
+		
+		try {
+			con = dataSource.getConnection();
+			
+			StringBuffer sql = new StringBuffer(500);
+			sql.append("SELECT * ");
+			sql.append("FROM ");
+
+			sql.append("( ");
+			sql.append("	SELECT ");
+			sql.append("		rownum idx, s.* ");
+			sql.append("	FROM ");
+
+			sql.append("	( ");
+			sql.append("		SELECT ");
+			sql.append("			N_No,N_TITLE,N_CONTENT, ");
+			sql.append("			N_DATE ");
+			sql.append("		FROM ");
+			sql.append("			notice ");
+			sql.append("	) s ");
+
+			sql.append(") ");
+
+			sql.append("WHERE idx >= ? AND idx <= ? ");
+			
+			pstmt = con.prepareStatement(sql.toString());
+			pstmt.setInt(1, start);
+			pstmt.setInt(2, last);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				Notice notice = new Notice();
+				notice.setN_no(rs.getInt(2));
+				notice.setN_title(rs.getString(3));
+				notice.setN_content(rs.getString(4));
+				notice.setN_date(rs.getDate(5));
+				
+				notices.add(notice);
+			}
+			
+		}finally {
+			if(pstmt != null)
+				try {
+					pstmt.close();
+				}catch(Exception ex) {
+					
+				}
+			if (con != null)
+				try {
+					con.close();
+				} catch (Exception ex) {
+				}
+			
+		}
+		
+		return notices;
+	}
+	
+	//게시물 총 건수
+	public int getNoticeCount() throws Exception{
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		int count = 0;
+		try {
+			con = dataSource.getConnection();
+			String sql = "SELECT COUNT(*) FROM notice";
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			if (rs.next())
+				count = rs.getInt(1);
+
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+			} catch (Exception ex) {
+			}
+			try {
+				if (pstmt != null)
+					pstmt.close();
+			} catch (Exception ex) {
+			}
+			try {
+				if (con != null)
+					con.close();
+			} catch (Exception ex) {
+			}
+		}
+		return count;
+	}
+	
+	
 }
