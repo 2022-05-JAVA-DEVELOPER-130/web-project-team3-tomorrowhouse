0b30919c1e455bcb337858f75ae1809ea671128a
yoon <yoon@localhost>
2022-08-07 17:31:43 +0900

    Qna_list 구현

--- src/main/java/com/itwill/shop/dao/QnaDao.java
+++ src/main/java/com/itwill/shop/dao/QnaDao.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Properties;
 
@@ -31,57 +32,54 @@ public class QnaDao {
 		}
 	
 	public int createQna(Qna qna) throws Exception{
-		
-		Connection con = dataSource.getConnection();
-		PreparedStatement pstmt = con.prepareStatement(QnaSQL.QNA_INSERT);
-		pstmt.setString(1, qna.getQ_title());
-		pstmt.setString(2, qna.getQ_content());
-		pstmt.setString(3, qna.getU_id());
-				
-		int createRowCount = pstmt.executeUpdate();
-		
-		con.close();
-		return createRowCount;
-	}
-	
-	public int removeQna(int q_no) throws Exception{
-
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		int count = 0;
+			Connection con = null;
+			PreparedStatement pstmt = null;
 		try {
 			con = dataSource.getConnection();
-
-			pstmt = con.prepareStatement(QnaSQL.QNA_DELETE_NO);
-			pstmt.setInt(1, q_no);
-			count = pstmt.executeUpdate();
-		} finally {
-			try {
-				if (pstmt != null)
-					pstmt.close();
-			} catch (Exception ex) {
-			}
-			try {
-				if (con != null)
-					con.close();;
-			} catch (Exception ex) {
-			}
-		}
-		return count;
-
-	}
-	
-	public int updateQna(Qna qna) throws Exception{
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		int count = 0;
-		try {
-			con = dataSource.getConnection();
-			pstmt = con.prepareStatement(QnaSQL.QNA_UPDATE);
+			pstmt = con.prepareStatement(QnaSQL.QNA_CREATE);
 			pstmt.setString(1, qna.getQ_title());
-			pstmt.setString(2, qna.getQ_content());
-			pstmt.setInt(3, qna.getQ_no());
+			pstmt.setString(2, qna.getU_id());
+			pstmt.setString(3, qna.getQ_content());
+					
+			int createRowCount = pstmt.executeUpdate();
+			
+			con.close();
+			return createRowCount;
+		} finally {
+		con.close();
+		}
+	}
+	
+	/**
+	 * 답글 게시물을 추가하는 메써드
+	 */
+	public int createQnaReply(Qna qna) throws Exception{
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		int count = 0;
+		try {
+			// 댓글을 작성할 대상글(원글)의 정보를 조회
+			Qna temp = this.findQna(qna.getQ_no());
+
+			// 영향을 받는 기존 글들의 논리적인 순서 번호 변경
+			con = dataSource.getConnection();
+			pstmt = con.prepareStatement(QnaSQL.QNA_SORT);
+			pstmt.setInt(1, temp.getQ_step());// step 번호
+			pstmt.setInt(2, temp.getQ_groupno());// group 번호
+			pstmt.executeUpdate();
+			pstmt.close();
+
+			// 댓글 삽입
+			pstmt = con.prepareStatement(QnaSQL.QNA_REPLY_CREATE);
+			pstmt.setString(1, qna.getQ_title());// 제목
+			pstmt.setString(2, qna.getU_id());// 작성자
+			pstmt.setString(3, qna.getQ_content());// 내용
+			pstmt.setInt(4, temp.getQ_groupno());// group no
+			pstmt.setInt(5, temp.getQ_step() + 1);// step
+			pstmt.setInt(6, temp.getQ_depth() + 1);// depth
+
 			count = pstmt.executeUpdate();
+	
 		} finally {
 			try {
 				if (pstmt != null)
@@ -90,13 +88,14 @@ public class QnaDao {
 			}
 			try {
 				if (con != null)
-					con.close();;
+					con.close();
 			} catch (Exception ex) {
 			}
 		}
 		return count;
+
 	}
-	
+
 	/**
 	 * 게시물 리스트를 반환(게시물시작번호,게시물끝번호)
 	 */
@@ -111,8 +110,6 @@ public class QnaDao {
 		try {
 			con = dataSource.getConnection();
 			// String sql="select boardno,title,groupno,step,depth from board order by groupno desc";
-
-			
 
 			pstmt = con.prepareStatement(QnaSQL.QNA_SELECT_ALL);
 			pstmt.setInt(1, start);
@@ -148,6 +145,117 @@ public class QnaDao {
 		// System.out.println(boards);
 		return qnas;
 	}
+
+	/*
+	 * 
+	 * 
+	 */
+	/**
+	 * 답변게시물 존재여부확인메쏘드
+	 */
+	public boolean countReplay(Qna qna) throws SQLException {
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		Boolean isExist = false;
+		int cnt = 0;
+		try {
+			con = dataSource.getConnection();
+			pstmt = con.prepareStatement(QnaSQL.QNA_REPLY_CHECK);
+			pstmt.setInt(1, qna.getQ_groupno());
+			pstmt.setInt(2, qna.getQ_depth());
+			pstmt.setInt(3, qna.getQ_step());
+
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				cnt = rs.getInt("cnt");
+			}
+			if (cnt > 1) {
+				isExist = true;
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+			} catch (Exception ex) {
+			}
+			try {
+				if (pstmt != null)
+					pstmt.close();
+			} catch (Exception ex) {
+			}
+			try {
+				if (con != null)
+					con.close();
+			} catch (Exception ex) {
+			}
+		}
+		return isExist;
+
+	}
+
+	/**
+	 * 게시물을 삭제하는 메써드.
+	 */
+	public int removeQna(int q_no) throws Exception{
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		int count = 0;
+		try {
+			con = dataSource.getConnection();
+			pstmt = con.prepareStatement(QnaSQL.QNA_DELETE_NO);
+			pstmt.setInt(1, q_no);
+			count = pstmt.executeUpdate();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+			} catch (Exception ex) {
+			}
+			try {
+				if (con != null)
+					con.close();;
+			} catch (Exception ex) {
+			}
+		}
+		return count;
+
+	}
+
+	/**
+	 * 게시물내용을 수정하는 메써드.
+	 */
+	public int updateQna(Qna qna) throws Exception{
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		int count = 0;
+		try {
+			con = dataSource.getConnection();
+			pstmt = con.prepareStatement(QnaSQL.QNA_UPDATE);
+			pstmt.setString(1, qna.getQ_title());
+			pstmt.setString(2, qna.getQ_content());
+			pstmt.setString(3, qna.getU_id());
+			pstmt.setInt(4, qna.getQ_no());
+			count = pstmt.executeUpdate();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+			} catch (Exception ex) {
+			}
+			try {
+				if (con != null)
+					con.close();;
+			} catch (Exception ex) {
+			}
+		}
+		return count;
+	}
+
 	/**
 	 * 게시물 번호에 해당하는 게시물 정보를 반환하는 메써드.
 	 */
@@ -195,4 +303,75 @@ public class QnaDao {
 		return qna;
 
 	}
+
+	/**
+	 * 게시물 조회수를 1 증가.
+	 */
+	public void increaseReadCount(int countNo) throws Exception {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			con = dataSource.getConnection();
+			pstmt = con.prepareStatement(QnaSQL.QNA_READCOUNTUP);
+			pstmt.setInt(1, countNo);
+			pstmt.executeUpdate();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+			} catch (Exception ex) {
+			}
+			try {
+				if (con != null)
+					con.close();
+			} catch (Exception ex) {
+			}
+		}
+	}
+
+	/**
+	 * 게시물 총 건수를 조회, 반환
+	 */
+	public int getQnaCount() throws Exception{
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		int count = 0;
+		try {
+			con = dataSource.getConnection();
+			pstmt = con.prepareStatement(QnaSQL.QNA_COUNT);
+			rs = pstmt.executeQuery();
+			if (rs.next())
+				count = rs.getInt(1);
+
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+			} catch (Exception ex) {
+			}
+			try {
+				if (pstmt != null)
+					pstmt.close();
+			} catch (Exception ex) {
+			}
+			try {
+				if (con != null)
+					con.close();
+			} catch (Exception ex) {
+			}
+		}
+		return count;
+	}
+
 }
+
+	
+	
+	
+	
+	
+	
+	
+	
