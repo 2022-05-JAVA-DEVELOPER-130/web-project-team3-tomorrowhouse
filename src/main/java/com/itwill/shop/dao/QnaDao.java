b51e7cabd0dee7670d1ad8189e15322e12cafdd4
yoon <yoon@localhost>
2022-08-07 14:18:10 +0900

    QNA 진행중..

--- src/main/java/com/itwill/shop/dao/QnaDao.java
+++ src/main/java/com/itwill/shop/dao/QnaDao.java
@@ -4,13 +4,14 @@ import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.Properties;
 
 import javax.sql.DataSource;
 
 import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
 
-import com.itwill.shop.dto.Qna;
+import com.itwill.shop.dto.qna.Qna;
 import com.itwill.shop.sql.QnaSQL;
 
 public class QnaDao {
@@ -43,7 +44,155 @@ public class QnaDao {
 		return createRowCount;
 	}
 	
+	public int removeQna(int q_no) throws Exception{
 
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		int count = 0;
+		try {
+			con = dataSource.getConnection();
+
+			pstmt = con.prepareStatement(QnaSQL.QNA_DELETE_NO);
+			pstmt.setInt(1, q_no);
+			count = pstmt.executeUpdate();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+			} catch (Exception ex) {
+			}
+			try {
+				if (con != null)
+					con.close();;
+			} catch (Exception ex) {
+			}
+		}
+		return count;
+
+	}
 	
+	public int updateQna(Qna qna) throws Exception{
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		int count = 0;
+		try {
+			con = dataSource.getConnection();
+			pstmt = con.prepareStatement(QnaSQL.QNA_UPDATE);
+			pstmt.setString(1, qna.getQ_title());
+			pstmt.setString(2, qna.getQ_content());
+			pstmt.setInt(3, qna.getQ_no());
+			count = pstmt.executeUpdate();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+			} catch (Exception ex) {
+			}
+			try {
+				if (con != null)
+					con.close();;
+			} catch (Exception ex) {
+			}
+		}
+		return count;
+	}
 	
+	/**
+	 * 게시물 리스트를 반환(게시물시작번호,게시물끝번호)
+	 */
+	public ArrayList<Qna> findQnaList(int start, int last) throws Exception{
+		System.out.println("" + start + " ~ " + last);
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;// 조회 결과에 접근하는 참조 변수
+		// 데이터베이스의 데이터를 읽어서 저장할 객체 컬렉션
+		ArrayList<Qna> qnas = new ArrayList<Qna>();
+
+		try {
+			con = dataSource.getConnection();
+			// String sql="select boardno,title,groupno,step,depth from board order by groupno desc";
+
+			
+
+			pstmt = con.prepareStatement(QnaSQL.QNA_SELECT_ALL);
+			pstmt.setInt(1, start);
+			pstmt.setInt(2, last);
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				Qna qna = new Qna();
+				qna.setQ_no(rs.getInt(2));
+				qna.setQ_title(rs.getString(3));
+				qna.setU_id(rs.getString(4));
+				qna.setQ_date(rs.getDate(5));
+				qna.setQ_readcount(rs.getInt(6));
+				qna.setQ_groupno(rs.getInt(7));
+				qna.setQ_step(rs.getInt(8));
+				qna.setQ_depth(rs.getInt(9));
+
+				qnas.add(qna);
+			}
+		} finally {
+			// 6. 연결닫기
+			if (pstmt != null)
+				try {
+					pstmt.close();
+				} catch (Exception ex) {
+				}
+			if (con != null)
+				try {
+					con.close();
+				} catch (Exception ex) {
+				}
+		}
+		// System.out.println(boards);
+		return qnas;
+	}
+	/**
+	 * 게시물 번호에 해당하는 게시물 정보를 반환하는 메써드.
+	 */
+	public Qna findQna(int q_no) throws Exception {
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		Qna qna = null;
+
+		try {
+			con = dataSource.getConnection();
+			pstmt = con.prepareStatement(QnaSQL.QNA_SELECT_BY_NO);
+			pstmt.setInt(1, q_no);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				qna = new Qna();
+				qna.setQ_no(rs.getInt(1));
+				qna.setQ_title(rs.getString(2));
+				qna.setU_id(rs.getString(3));
+				qna.setQ_content(rs.getString(4));
+				qna.setQ_date(rs.getDate(5));
+				qna.setQ_readcount(rs.getInt(6));
+				qna.setQ_groupno(rs.getInt(7));
+				qna.setQ_step(rs.getInt(8));
+				qna.setQ_depth(rs.getInt(9));
+			}
+		}finally {
+			try {
+				if (rs != null)
+					rs.close();
+			} catch (Exception ex) {
+			}
+			try {
+				if (pstmt != null)
+					pstmt.close();
+			} catch (Exception ex) {
+			}
+			try {
+				if (con != null)
+					con.close();
+			} catch (Exception ex) {
+			}
+		}
+		return qna;
+
+	}
 }
--- src/main/java/com/itwill/shop/sql/QnaSQL.java
+++ src/main/java/com/itwill/shop/sql/QnaSQL.java
@@ -2,27 +2,33 @@ package com.itwill.shop.sql;
 
 public class QnaSQL {
 	public static final String QNA_INSERT =
-			"insert into qna(q_no, q_title, q_content, q_groupno, q_step, q_depth, u_id) values(qna_q_no_seq.nextval, 't', 't', qna_q_no_seq.currval, 1, 0, 'test1');";
-	
+			"insert into qna(q_no, q_title, q_content, q_groupno, q_step, q_depth, u_id) values(qna_q_no_seq.nextval, ?, ?, qna_q_no_seq.currval, 1, 0, ?)";
+
 	public static final String QNA_SELECT_ALL = 
-			"select * from qna;";
-	// 모든 문의 리스트 출력 >> qna_list.jsp 출력
+			"select * FROM (SELECT rownum idx, s.* FROM(SELECT q_no, q_title, u_id, q_date, q_readcount, q_groupno, q_step, q_depth FROM qna ORDER BY q_groupno DESC, q_step ASC)s)WHERE idx >= ? AND idx <= ?)";
 	
+	public static final String QNA_DELETE_NO = "DELETE qna WHERE q_no = ?";
+	
+	public static final String QNA_UPDATE = "UPDATE qna SET q_title = ?, q_content = ? WHERE q_no = ?";
+	
+	public static final String QNA_SELECT_BY_NO = "SELECT q_no, q_title, u_id, q_content, q_date, q_readcount, q_groupno, q_step, q_depth FROM board WHERE q_no = ?";
+/*	
 	public static final String QNA_SELECT_ALL_USER_BY_ID = 
-			"select * from qna where u_id = ?;";
+			"select * from qna where u_id = ?";
 	// test1의 모든 문의 리스트 >> MyPage에서 출력
 
-	/*
+	
 	public static final String QNA_SELECT_BY_NO_USER_BY_ID = 
 			"select * from qna where u_id = ? and q_no = ?;";
 	// test1의 문의 pk로 1개 출력 >> ??????
-	*/
+	
 	
 	public static final String QNA_UPDATE_BY_NO_AND_ID = 
-			"update qna set q_title = ?, q_content = ?, q_category = ? where u_id = ? and q_no = ?;";
+			"update qna set q_title = ?, q_content = ?, q_category = ? where u_id = ? and q_no = ?";
 	// test2의 문의 1개 pk로 변경
 	
 	public static final String QNA_DELETE_BY_NO_AND_ID = 
-			"delete from qna where u_id=? and q_no = ?;";
+			"delete from qna where u_id=? and q_no = ?";
 	// test1의 문의 pk로 1개 삭제
+*/
 }
